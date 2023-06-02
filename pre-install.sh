f96ee57a838fac2d0fbe0483f8157bb7acd92d61
Johannes Ogoniak <johannes.ogoniak@andsafe.de>
2023-06-02 11:34:21 +0200

    Add raycast to brewfile. Change pre install scripts.

--- pre-install.sh
+++ pre-install.sh
@@ -1,7 +1,7 @@
 # Check if Homebrew is installed
 if [ ! -f "`which brew`" ]; then
   echo 'Installing homebrew'
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo 'Updating homebrew'
   brew update
@@ -12,5 +12,5 @@ brew tap homebrew/bundle  # Install Homebrew Bundle
 OMZDIR="$HOME/.oh-my-zsh"
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo 'Installing oh-my-zsh'
-  /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
